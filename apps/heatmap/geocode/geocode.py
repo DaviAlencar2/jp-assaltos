001e8cd9cfb3471a1a1dc8381bfd75bd19e3fdb7
DaviAlencar2 <davilt.alencar@gmail.com>
2026-02-15 16:43:46 -0300

    fix: problema ao carregar variaveis de ambiente

--- apps/heatmap/geocode/geocode.py
+++ apps/heatmap/geocode/geocode.py
@@ -2,12 +2,11 @@ from opencage.geocoder import OpenCageGeocode
 from dotenv import load_dotenv
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv()
 
 API_KEY = os.getenv("OPENCAGE_API_KEY")
 
-def geocode_address(address):
+def geocode_address(address):       
     geocoder = OpenCageGeocode(API_KEY)
     data = geocoder.geocode(address)
     
